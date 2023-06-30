bdc89dfabcdd3274c0802fe57e9bdc9cf856da95
Sanghun Oh <otter.oh@gmail.com>
2023-07-01 01:10:48 +0900

    add somethings

--- src/main/java/com/yojulab/study_springboot/restapis/HelloWorldController.java
+++ src/main/java/com/yojulab/study_springboot/restapis/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +18,14 @@ public class HelloWorldController {
     HelloWorldService helloWorldService;
 
     @GetMapping("/helloWorld")
-    public int helloWorld(){
+    public int helloWorld() {
         return 0;
     }
+
     // /helloWorldGetRequest?Name=yojulab&Id=U-01
     // /helloWorldGetRequest/yojulab/U-01
     @GetMapping("/helloWorldGetRequest/{name}/{Id}")
-    public int helloWorldGetRequest(@PathVariable String name, @PathVariable String Id){
+    public int helloWorldGetRequest(@PathVariable String name, @PathVariable String Id) {
         return 0;
     }
 
@@ -33,8 +35,8 @@ public class HelloWorldController {
     // &SN=1
     // /helloWorldResponse/1/10/1
     @GetMapping("/helloWorldResponse/{currentPage}/{perPage}/{SN}")
-    public ResponseEntity<Object> helloWorldResponse(@PathVariable String currentPage
-                    , @PathVariable String perPage, @PathVariable String SN){
+    public ResponseEntity<Object> helloWorldResponse(@PathVariable String currentPage, @PathVariable String perPage,
+            @PathVariable String SN) {
         // "spm_row": 471, "SN": 1, "CMPNM": "로이유통", "RDNMADR": null
         HashMap resultMap = new HashMap<>();
         resultMap.put("spm_row", 471);
@@ -47,19 +49,23 @@ public class HelloWorldController {
 
     // /helloWorldResponseList/1/10/1
     @GetMapping("/helloWorldResponseList/{currentPage}/{perPage}/{SN}")
-    public ResponseEntity<Object> helloWorldResponseList(@PathVariable String currentPage
-                    , @PathVariable String perPage, @PathVariable String SN){
+    public ResponseEntity<Object> helloWorldResponseList(@PathVariable String currentPage, @PathVariable String perPage,
+            @PathVariable String SN) {
         // "spm_row": 471, "SN": 1, "CMPNM": "로이유통", "RDNMADR": null
         // "spm_row": 571, "SN": 2, "CMPNM": "의료유통", "RDNMADR": 3
         ArrayList arrayList = new ArrayList<>();
         HashMap resultMap = new HashMap<>();
-        resultMap.put("spm_row", 471);        resultMap.put("SN", 1);
-        resultMap.put("CMPNM", "로이유통");    resultMap.put("RDNMADR", null);
+        resultMap.put("spm_row", 471);
+        resultMap.put("SN", 1);
+        resultMap.put("CMPNM", "로이유통");
+        resultMap.put("RDNMADR", null);
         arrayList.add(resultMap);
 
         resultMap = new HashMap<>();
-        resultMap.put("spm_row", 571);        resultMap.put("SN", 2);
-        resultMap.put("CMPNM", "의료유통");    resultMap.put("RDNMADR", 3);
+        resultMap.put("spm_row", 571);
+        resultMap.put("SN", 2);
+        resultMap.put("CMPNM", "의료유통");
+        resultMap.put("RDNMADR", 3);
         arrayList.add(resultMap);
 
         return ResponseEntity.ok().body(arrayList);
@@ -67,8 +73,8 @@ public class HelloWorldController {
 
     // /helloWorldResponseWithService/1/10/1
     @GetMapping("/helloWorldResponseWithService/{currentPage}/{perPage}/{SN}")
-    public ResponseEntity<Object> helloWorldResponseWithService(@PathVariable String currentPage
-                    , @PathVariable String perPage, @PathVariable String SN){
+    public ResponseEntity<Object> helloWorldResponseWithService(@PathVariable String currentPage,
+            @PathVariable String perPage, @PathVariable String SN) {
         ArrayList arrayList = new ArrayList<>();
         arrayList = helloWorldService.fakeSelect(currentPage, perPage);
         return ResponseEntity.ok().body(arrayList);
@@ -76,10 +82,23 @@ public class HelloWorldController {
 
     // /helloWorldResponseFake/C001
     @GetMapping("/helloWorldResponseFake/{companyId}")
-    public ResponseEntity<Object> helloWorldResponseFake(@PathVariable String companyId){
+    public ResponseEntity<Object> helloWorldResponseFake(@PathVariable String companyId) {
         ArrayList arrayList = new ArrayList<>();
         helloWorldService.fakeSelect(companyId);
         return ResponseEntity.ok().body(arrayList);
     }
 
+    // /helloWorldResponseFakeWithExcepation/C001
+    @GetMapping("/helloWorldResponseFakeWithExcepation/{companyId}")
+    public ResponseEntity<Object> helloWorldResponseFakeWithExcepation(@PathVariable String companyId) {
+        ArrayList arrayList = new ArrayList<>();
+        try {
+            helloWorldService.fakeSelect(companyId);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().body("Error: ");
+
+        }
+        return ResponseEntity.ok().body(arrayList);
+    }
 }
