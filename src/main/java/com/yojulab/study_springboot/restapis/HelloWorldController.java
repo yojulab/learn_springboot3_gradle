6d479292a2275b14859d2104819a5696ecc654fe
yojulab <otter.oh@gmail.com>
2023-06-30 15:26:15 +0900

    first commit

---
+++ src/main/java/com/yojulab/study_springboot/restapis/HelloWorldController.java
@@ -0,0 +1,61 @@
+package com.yojulab.study_springboot.restapis;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/helloWorld")
+    public int helloWorld(){
+        return 0;
+    }
+    // /helloWorldGetRequest?Name=yojulab&Id=U-01
+    // /helloWorldGetRequest/yojulab/U-01
+    @GetMapping("/helloWorldGetRequest/{name}/{Id}")
+    public int helloWorldGetRequest(@PathVariable String name, @PathVariable String Id){
+        return 0;
+    }
+
+    // ?serviceKey=BoygPZjC27pxm92hSposjnSob2u36vziS1rzIzxkrL9QxmlhB0SMARwLfNlBE3wrE7nnw34zLmmv0a6amvW4xg%3D%3D
+    // &currentPage=1
+    // &perPage=10
+    // &SN=1
+    // /helloWorldResponse/1/10/1
+    @GetMapping("/helloWorldResponse/{currentPage}/{perPage}/{SN}")
+    public ResponseEntity<Object> helloWorldResponse(@PathVariable String currentPage
+                    , @PathVariable String perPage, @PathVariable String SN){
+        // "spm_row": 471, "SN": 1, "CMPNM": "로이유통", "RDNMADR": null
+        HashMap resultMap = new HashMap<>();
+        resultMap.put("spm_row", 471);
+        resultMap.put("SN", 1);
+        resultMap.put("CMPNM", "로이유통");
+        resultMap.put("RDNMADR", null);
+
+        return ResponseEntity.ok().body(resultMap);
+    }
+
+    // /helloWorldResponseList/1/10/1
+    @GetMapping("/helloWorldResponseList/{currentPage}/{perPage}/{SN}")
+    public ResponseEntity<Object> helloWorldResponseList(@PathVariable String currentPage
+                    , @PathVariable String perPage, @PathVariable String SN){
+        // "spm_row": 471, "SN": 1, "CMPNM": "로이유통", "RDNMADR": null
+        // "spm_row": 571, "SN": 2, "CMPNM": "의료유통", "RDNMADR": 3
+        ArrayList arrayList = new ArrayList<>();
+        HashMap resultMap = new HashMap<>();
+        resultMap.put("spm_row", 471);        resultMap.put("SN", 1);
+        resultMap.put("CMPNM", "로이유통");    resultMap.put("RDNMADR", null);
+        arrayList.add(resultMap);
+
+        resultMap = new HashMap<>();
+        resultMap.put("spm_row", 571);        resultMap.put("SN", 2);
+        resultMap.put("CMPNM", "의료유통");    resultMap.put("RDNMADR", 3);
+        arrayList.add(resultMap);
+
+        return ResponseEntity.ok().body(arrayList);
+    }
+}
