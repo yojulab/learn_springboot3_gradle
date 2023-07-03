3b6c3de7110f1b596a4f4a6aa5af0184f819693f
yojulab <otter.oh@gmail.com>
2023-07-03 18:19:16 +0900

    add somethings

---
+++ src/main/java/com/yojulab/study_springboot/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.yojulab.study_springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class MainController {
+    @GetMapping({"/"})      // 일반과 관리자가 접속 가능
+    public ModelAndView main(ModelAndView modelAndView){
+
+        String viewName = "/WEB-INF/views/main.jsp";
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+    
+    @GetMapping({"/admin"})     // 관리자 접속하는 곳
+    public ModelAndView admin(ModelAndView modelAndView){
+        String viewName = "/WEB-INF/views/admin.jsp";
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>Main</div>
+    <hr />
+    <div>Spring Security Area</div>
+    <hr />
+    <div>authorizeRequests() in SecurityConfiguration.java</div>
+    <div>
+        <a href="/admin">admin.jsp</a>
+    </div>
+    <div>
+        <a href="/admin/read">/admin/*.jsp with Role</a>
+    </div>
+    <div>
+        <a href="/manager/read">/manager/*.jsp with Role</a>
+    </div>
+
+</body>
+
+</html>
